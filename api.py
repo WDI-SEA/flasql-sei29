47271161d73f214f0380e71ab5b74af52a7ded03
009kings <009kings@gmail.com>
2020-03-31 17:08:52 -0700

    nice n functional and clear

--- api.py
+++ api.py
@@ -1,50 +1,31 @@
 from models import app
-from helpers import InvalidUsage
 from flask import jsonify, request
 from crud.user_crud import get_all_users, get_user, create_user, destroy_user, update_user
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+  app.logger.error('Unhandled Exception: %s', (e))
+  message_str = e.__str__()
+  return jsonify(message=message_str.split(':')[0])
 
 @app.route('/users', methods=['GET', 'POST'])
 def user_index_create():
   if request.method == 'GET':
-    try:
-      return get_all_users()
-    except Exception as error:
-      print(error.to_dict())
-      return error.client_view()
+    return get_all_users()
   if request.method == 'POST':
-    try:
-      return create_user(
-        name=request.form['name'], 
-        email=request.form['email'], 
-        bio=request.form['bio']
-      )
-    except Exception as error:
-      print(error.to_dict())
-      return error.client_view()
+    return create_user()
 
 
 @app.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def user_show_update_delete(id):
   if request.method == 'GET':
-    try:
-      return get_user(id)
-    except Exception as error:
-      print(error.to_dict())
-      return error.client_view()
+    return get_user(id)
   if request.method == 'PUT':
-    try:
-      return update_user(
-        id=id, 
-        name=request.form['name'],
-        email=request.form['email'],
-        bio=request.form['bio']
-      )
-    except Exception as error:
-      print(error.to_dict())
-      return error.client_view()
+    return update_user(
+      id=id, 
+      name=request.form['name'],
+      email=request.form['email'],
+      bio=request.form['bio']
+    )
   if request.method == 'DELETE':
-    try:
-      return destroy_user(id)
-    except Exception as error:
-      print(error.to_dict())
-      return error.client_view()
+    return destroy_user(id)
--- crud/user_crud.py
+++ crud/user_crud.py
@@ -1,69 +1,48 @@
 from flask import jsonify, redirect
-from helpers import InvalidUsage
 from models import db, User
 
 # Index
 def get_all_users():
-  try:
-    all_users = User.query.all()
+  all_users = User.query.all()
+  if len(all_users) > 0:
     results = [user.as_dict() for user in all_users]
-    return jsonify(results)
-  except Exception as error:
-    print(error.to_dict())
-    return error.client_view()
+  else:
+    results = []
+  return jsonify(results)
 
 # Show
 def get_user(id):
-  try:
-    user = User.query.get(id)
-    print(user)
-    if user:
-      return jsonify(user.as_dict())
-    else:
-      raise InvalidUsage('No user at that id')
-  except Exception as error:
-    print(error.to_dict())
-    return error.client_view()
+  user = User.query.get(id)
+  if user:
+    return jsonify(user.as_dict())
+  else:
+    raise Exception('No User at id {}'.format(id))
 
 # Create
 def create_user(name, email, bio):
-  try:
-    new_user = User(name=name, email=email, bio=bio or None)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify(new_user.as_dict())
-  except Exception as error:
-    print(error.to_dict())
-    return error.client_view()
+  new_user = User(name=name, email=email, bio=bio or None)
+  db.session.add(new_user)
+  db.session.commit()
+  return jsonify(new_user.as_dict())
 
 # Update function!
 def update_user(id, name, email, bio):
-  try:
-    user = User.query.get(id)
-    if user:
-      user.email = email or user.email
-      user.name = name or user.name
-      user.bio = bio or user.bio
-      db.session.commit()
-      return jsonify(user.as_dict())
-      # return redirect(f'/users/{id}')
-    else:
-      # return jsonify(error.to_dict())
-      raise InvalidUsage('No User here!')
-  except Exception as error:
-    print(error.to_dict())
-    return error.client_view()
+  user = User.query.get(id)
+  if user:
+    user.email = email or user.email
+    user.name = name or user.name
+    user.bio = bio or user.bio
+    db.session.commit()
+    return jsonify(user.as_dict())
+  else:
+    raise Exception('No User at id {}'.format(id))
 
 # Destroy
 def destroy_user(id):
-  try:
-    user = User.query.get(id)
-    if user:
-      db.session.delete(user)
-      db.session.commit()
-      return redirect('/users')
-    else:
-      raise InvalidUsage('No user here')
-  except Exception as error:
-    print(error.to_dict())
-    return error.client_view()
+  user = User.query.get(id)
+  if user:
+    db.session.delete(user)
+    db.session.commit()
+    return redirect('/users')
+  else:
+    raise Exception('No User at id {}'.format(id))
